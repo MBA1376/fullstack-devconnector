591442c123a1b622fc8a7c340cc508d477c5e894
unknown <Asus@example.com>
2019-01-05 20:35:02 +0330

    Profile api routes

---
+++ validation/education.js
@@ -0,0 +1,35 @@
+const validator = require('validator');
+const isEmpty = require('./is-empty');
+
+const validateEducationInput = (data) => {
+    let errors = {};
+    
+    data.school = isEmpty(data.school) ? '' : data.school;
+    data.degree = isEmpty(data.degree) ? '' : data.degree;
+    data.fieldofstudy = isEmpty(data.fieldofstudy) ? '' : data.fieldofstudy;
+    data.from = isEmpty(data.from) ? '' : data.from;
+    
+
+    if(validator.isEmpty(data.school)) {
+        errors.school = 'School is required';
+    }
+    if(validator.isEmpty(data.degree)) {
+        errors.degree = 'degree field is required';
+    }
+    if(validator.isEmpty(data.fieldofstudy)) {
+        errors.fieldofstudy = 'field of study field is required';
+    }
+    if(validator.isEmpty(data.from)) {
+        errors.from = 'from date field is required';
+    }
+
+    
+    
+
+    return {
+        errors ,
+        isValid : isEmpty(errors)
+    }
+}
+
+module.exports = validateEducationInput ;
