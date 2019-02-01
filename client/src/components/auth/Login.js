2cade09ddaf14c9ce24cfbc52182a5b71d992971
unknown <Asus@example.com>
2019-02-01 21:31:02 +0330

    add react-router-dom and set routes

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+
+class Login extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Login</h1>
+            </div>
+        );
+    }
+}
+
+export default Login;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,14 @@
+import React , {Component} from 'react';
+
+class Register extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Register</h1>
+            </div>
+        );
+    }
+}
+
+export default Register;
